c06a798e6c654630d3ec724776c8b68152efe44f
anthony <anthony.gilliam@gmail.com>
2015-01-30 01:06:14 -0500

    Bundling _Layout view javaScript files and moving jquery.ui to index page.

--- Projects/LikeMyDessert.Web/App_Start/BundleConfig.cs
+++ Projects/LikeMyDessert.Web/App_Start/BundleConfig.cs
@@ -10,13 +10,18 @@ namespace LikeMyDessert.Web.App_Start
     {
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/Bundle/Scripts/jquery").Include(
+            bundles.Add(new ScriptBundle("~/Bundle/Scripts/Layout").Include(
+                "~/Scripts/common/less-1.1.3.min.js",
                 "~/Scripts/jquery/jquery-{version}.js",
-                "~/Scripts/jquery/modernizr-1.7.js")
+                "~/Scripts/jquery/modernizr-1.7.js",
+                "~/Scripts/global.js")
             );
 
             bundles.Add(new ScriptBundle("~/Bundle/Scripts/jquery-ui").Include(
-                "~/Scripts/jquery/jquery-ui-1.8.18.custom.min.js")
+                "~/Scripts/jquery/jquery-ui-1.8.18.custom.min.js",
+                "~/Scripts/jquery/jquery.form.js",
+                "~/Scripts/jquery/jquery.validate.js",
+                "~/Scripts/jquery/jquery.validate.unobtrusive.js")
             );
 
             bundles.Add(new StyleBundle("~/Bundle/Content/css/Layout").Include(
