9df9c531e8dfc29b9be19c2c8ab1faf4d67f5505
Anthony <anthony.gilliam@gmail.com>
2018-01-13 17:48:59 -0500

    Turning NavBar in to compose element and starting to work on Post custom element

--- src/app.js
+++ src/app.js
@@ -4,15 +4,19 @@ import ImagesLoaded from 'imagesloaded'
 
 export class App {
     attached(){
+        this.layoutImages();
+    }
+
+    layoutImages() {
         $(function() {
             console.log('jQuery Ready')
             ImagesLoaded('#dessertsPane', () => {
-                    console.log("dessertsPane has finished loading!")
-                    const msnry = new Masonry('.wall', {
-                        itemSelector: '.post-wrapper',
-                        columnWidth: '.grid-sizer',
-                        percentPosition: true
-                    })
+                console.log("dessertsPane has finished loading!")
+                const msnry = new Masonry('.wall', {
+                    itemSelector: '.post-wrapper',
+                    columnWidth: '.grid-sizer',
+                    percentPosition: true
+                })
                 console.log('Masonry called')
             })
         })
